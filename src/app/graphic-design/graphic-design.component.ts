8e22c1e644f0302e17119e44478a65d27f310709
mdlawrenceusa <mdlawrence@me.com>
2020-11-10 15:50:31 -0500

    have to convert the JSON to an array

--- src/app/graphic-design/graphic-design.component.ts
+++ src/app/graphic-design/graphic-design.component.ts
@@ -11,28 +11,21 @@ import { CartService } from '../cart.service';
 })
 export class GraphicDesignComponent implements OnInit {
   portfolioItems ;
-  _albums = [] ;
+  _albums: any = [] ;
   constructor(
     private cartService: CartService,
     private _lightbox: Lightbox,
   ) {
- for (let i = 1; i <= 4; i++) {
-      const src = 'demo/img/image' + i + '.jpg';
-      const caption = 'Image ' + i + ' caption here';
-      const thumb = 'demo/img/image' + i + '-thumb.jpg';
-      const album = {
-         src: 'https://d3u4fnct1vpepk.cloudfront.net/website/war2/img/Visionaire.jpg',
-         caption: caption,
-         thumb: thumb
-      };
- 
-      this._albums.push(album);
-    }
+
 
    }
 
   ngOnInit() {
     this.portfolioItems = this.cartService.getPortfolio();
+    this.cartService.getPortfolio().subscribe(data =>{
+      console.log(data);
+      this._albums = data;
+    });
 
   }
 
